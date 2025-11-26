4aacd491914784fd70200ce23a1af7e564d2a83a
JalenEvans <jalenemmanuelevans@gmail.com>
2025-11-26 01:01:50 -0500

    Prevented duplicates from being inserted

--- backend/src/app/services/player_services.py
+++ backend/src/app/services/player_services.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+from psycopg2 import IntegrityError
 from sqlalchemy.orm import Session
 
 from ..models.Player import Player
@@ -8,7 +10,17 @@ def select_player_by_id(player_id: int, db: Session) -> Player:
     return player
 
 def insert_player(player_data: PlayerCreate, db: Session):
+    # Check if player already exists
+    existing_player = db.query(Player).filter(Player.player_id == player_data.player_id).first()
+    if existing_player:
+        raise HTTPException(status_code=400, detail="Player already exists")
+
     player = Player(**player_data.model_dump())
 
     db.add(player)
-    db.commit()
+    
+    try:
+        db.commit()
+    except IntegrityError as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Integrity error: " + str(e))
